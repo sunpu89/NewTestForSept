c959d434f00790dee57ce34767b1a41ea25fa866
sunpu <sunpu1995@outlook.com>
2025-09-18 19:33:06 +0800

    first initial

---
+++ cmake-build-debug/CMakeFiles/NewTestForSept.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NewTestForSept.dir/main.cpp.o"
+  "CMakeFiles/NewTestForSept.dir/main.cpp.o.d"
+  "NewTestForSept"
+  "NewTestForSept.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NewTestForSept.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/NewTestForSept.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for NewTestForSept.
+# This may be replaced when dependencies are built.
