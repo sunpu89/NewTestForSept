182258311c7297452dafade0e345df3c1995398b
sunpu <sunpu1995@outlook.com>
2025-09-18 19:55:13 +0800

    the first table

--- main.cpp
+++ main.cpp
@@ -1,20 +1,49 @@
 #include <iostream>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the
-    // <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code.
-        // We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/>
-        // breakpoint for you, but you can always add more by pressing
-        // <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+
+
+// 我需要一个结构体，来说明所有类型的桌子应该长什么样子
+struct Table {
+    std::string type;
+    int height;
+    std::string material; // 材质
+    int drawers; // 有无抽屉以及数量
+};
+
+// 构建桌子的函数
+void buildTable(const std::string& tableType) {
+    Table table;
+    // 餐桌、办公桌、咖啡桌
+    if (tableType == "dining") {
+        table.type = "餐桌";
+        table.height = 75;
+        table.material = "桐木";
+        table.drawers = 0;
+        std::cout << "已构建 " << table.type << "，高度 " << table.height << "cm，材质 " << table.material << "，无抽屉" << std::endl;
+    } else if (tableType == "office") {
+        table.type = "办公桌";
+        table.height = 100;
+        table.material = "塑料";
+        table.drawers = 2;
+        std::cout << "已构建 " << table.type << "，高度 " << table.height << "cm，材质 " << table.material << "，有 " << table.drawers << " 个抽屉" << std::endl;
+    } else if (tableType == "coffee") {
+        table.type = "咖啡桌";
+        table.height = 80;
+        table.material = "木";
+        table.drawers = 0;
+        std::cout << "已构建 " << table.type << "，高度 " << table.height << "cm，材质 " << table.material << "，无抽屉" << std::endl;
+    } else {
+        std::cout << "Unknown table type: " << tableType << std::endl;
     }
+}
+
+int main() {
+
+    std::cout << "现在你可以造桌子了，请你选择你要造的桌子类型（dining/office/coffee）：" << std::endl;
+    std::string tableType;
+    std::cin >> tableType;
+    buildTable(tableType);
 
     return 0;
 }
